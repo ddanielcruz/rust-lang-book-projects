e7b2ceb75b6e423ec34d4408a1eaedcba7004e6e
Daniel Cruz <danielcunha54@gmail.com>
2023-07-16 22:01:01 -0300

    Hello World and Guessing Game projects

---
+++ hello_world/main.rs
@@ -0,0 +1,7 @@
+// Special function always executed first in a Rust program
+// Four spaces indentation standard
+fn main() {
+    // Function calls ending with "!" are Rust macros, but work similar to normal functions
+    println!("Hello, world!");
+}
+  
