a27cf211e3a3d19b9fb788dc9a9e518624773495
Daniel Cruz <danielcunha54@gmail.com>
2023-07-31 09:34:19 -0300

    Lifetimes

---
+++ lifetimes/src/main.rs
@@ -0,0 +1,44 @@
+#[allow(dead_code)]
+#[derive(Debug)]
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}    
+
+fn main() {
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is {}", result);
+
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
+    println!("{:?}", i);
+}
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+#[allow(dead_code)]
+fn longest_with_an_announcement<'a, T>(
+    x: &'a str,
+    y: &'a str,
+    ann: T,
+) -> &'a str 
+where
+    T: std::fmt::Display,
+{
+    println!("Announcement! {}", ann);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
